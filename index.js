6ad981fc4b3385b39a13e45277b8429dc41e07f0
zfabilar <simonfabila@yahoo.com.mx>
2021-06-17 16:15:06 -0500

    Agregar express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const op = require('./operaciones.js');
 const so = require('os')
 const colors = require('colors')
-
+const exp = require('express')
 
 
 console.log(so.platform().green)
@@ -11,3 +11,9 @@ console.log(op.restar(5,4))
 console.log(op.multiplicar(5,4))
 console.log(op.dividir(5,4))
 console.log(op.dividir(5,0))
+
+const server = exp();
+
+server.listen(3000,function(){
+    console.log("Servidor corriendo".red)
+})
